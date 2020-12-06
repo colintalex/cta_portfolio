b7b5a395922ea4d71efade941e6cd7c92444cfbe
Colin Alexander <colintalex@gmail.com>
2020-12-06 03:39:54 -0700

    Sets up React routing based on primary App component

--- app/javascript/components/Homepage.jsx
+++ app/javascript/components/Homepage.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Home = () => {
+const Homepage = () => {
     return (
         <div>
-            Hola
+            Hola 2
         </div>
     )
-};
+}
 
-export default Home;
+export default Homepage;
