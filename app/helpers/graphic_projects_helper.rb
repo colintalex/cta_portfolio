e30c336a8ec2df457d1d9fbc9f1d280fce4a40af
Colin Alexander <colintalex@gmail.com>
2020-12-07 23:18:54 -0700

    Mutes test files for possible future use

---
+++ app/helpers/graphic_projects_helper.rb
@@ -0,0 +1,2 @@
+module GraphicProjectsHelper
+end
--- spec/requests/homepage_request_spec.rb
+++ spec/requests/homepage_request_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe "Homepages", type: :request do
+# RSpec.describe "Homepages", type: :request do
 
-  xdescribe "GET /index" do
-    xit "returns http success" do
-      get "/homepage/index"
-      expect(response).to have_http_status(:success)
-    end
-  end
+#   xdescribe "GET /index" do
+#     xit "returns http success" do
+#       get "/homepage/index"
+#       expect(response).to have_http_status(:success)
+#     end
+#   end
 
-end
+# end
