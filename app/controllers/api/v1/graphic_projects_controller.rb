b51be275ad1ce4ef7a3a67de748d9eaed46288a9
Colin Alexander <colintalex@gmail.com>
2020-12-07 23:19:23 -0700

    TDD controller for graphicproject crud

---
+++ app/controllers/api/v1/graphic_projects_controller.rb
@@ -0,0 +1,54 @@
+class Api::V1::GraphicProjectsController < ApplicationController
+
+  def index
+    graphic_projects = GraphicProject.all
+
+    render json: GraphicProjectSerializer.new(graphic_projects)
+  end
+
+  def show
+    graphic_project = GraphicProject.find(params[:id].to_i)
+
+    if graphic_project
+      render json: GraphicProjectSerializer.new(graphic_project)
+    else
+      # render errors here
+    end
+  end
+
+  def create
+    project = GraphicProject.new(project_params)
+
+    if project.save
+      render json: GraphicProjectSerializer.new(project)
+    else
+      # render errors here
+    end
+  end
+
+  def update
+    graphic_project = GraphicProject.find(params[:id].to_i)
+    graphic_project.update(project_params)
+    if graphic_project.save
+      render json: GraphicProjectSerializer.new(graphic_project)
+    else
+      # errors here
+    end
+  end
+
+  def destroy
+    graphic_project = GraphicProject.find(params[:id].to_i)
+    if graphic_project
+      graphic_project.destroy
+      render json: 'Successfully deleted'
+    else
+      # errors
+    end
+  end
+
+  private
+
+  def project_params
+    params.permit(:title, :description, :image_path)
+  end
+end
---
+++ spec/requests/graphic_projects_request_spec.rb
@@ -0,0 +1,103 @@
+require 'rails_helper'
+
+RSpec.describe "GraphicProjects", type: :request do
+
+  let(:project) { 
+    GraphicProject.create(
+      title: 'Title3',
+      description: 'Content',
+      image_path: '../path'
+    )
+  }
+
+  before(:each) {
+      GraphicProject.create(
+      title: 'Title4',
+      description: 'Content',
+      image_path: '../path'
+    )
+  }
+
+  
+  describe "GET All CodeProjects" do
+    it "returns http success" do
+      get "/api/v1/graphic_projects"
+
+      expect(response).to have_http_status(:success)
+      resp = JSON.parse(response.body,:symbolize_names => true)
+
+      expect(resp[:data][0][:attributes]).to have_key(:title)
+      expect(resp[:data][0][:attributes]).to have_key(:description)
+      expect(resp[:data][0][:attributes]).to have_key(:image_path)
+    end
+  end
+
+  describe "GET single code project by id" do
+    it "returns http success" do
+      get "/api/v1/graphic_projects/#{project.id}"
+
+      expect(response).to have_http_status(:success)
+      resp = JSON.parse(response.body,:symbolize_names => true)
+
+      expect(resp[:data][:attributes]).to have_key(:title)
+      expect(resp[:data][:attributes]).to have_key(:description)
+      expect(resp[:data][:attributes]).to have_key(:image_path)
+    end
+  end
+
+  describe "POST Create new graphic project" do
+    it "returns http success" do
+      data = {
+      title: 'Title3',
+      description: 'Content',
+      image_path: './new/path'
+    }
+      post "/api/v1/graphic_projects/", :params => data
+
+      expect(response).to have_http_status(:success)
+      resp = JSON.parse(response.body, :symbolize_names => true)
+
+      expect(resp[:data][:attributes]).to have_key(:title)
+      expect(resp[:data][:attributes]).to have_key(:description)
+      expect(resp[:data][:attributes]).to have_key(:image_path)
+
+      expect(resp[:data][:attributes][:title]).to eql(data[:title])
+      expect(resp[:data][:attributes][:description]).to eql(data[:description])
+      expect(resp[:data][:attributes][:image_path]).to eql(data[:image_path])
+    end
+  end
+
+  describe "PUT update graphic project" do
+    it "returns http success" do
+    data = {
+      title: 'Title3',
+      description: 'Content',
+      image_path: 'url',
+      deploy_url: 'url',
+      technology: 'List'
+    }
+      put "/api/v1/graphic_projects/#{project.id}", :params => data
+
+      expect(response).to have_http_status(:success)
+      resp = JSON.parse(response.body,:symbolize_names => true)
+
+      expect(resp[:data][:attributes]).to have_key(:title)
+      expect(resp[:data][:attributes]).to have_key(:description)
+      expect(resp[:data][:attributes]).to have_key(:image_path)
+
+      expect(resp[:data][:attributes][:title]).to eql(data[:title])
+      expect(resp[:data][:attributes][:description]).to eql(data[:description])
+      expect(resp[:data][:attributes][:image_path]).to eql(data[:image_path])
+    end
+  end
+
+  describe "DELETE destroy graphic project" do
+    it "returns http success" do
+      delete "/api/v1/graphic_projects/#{project.id}"
+
+      expect(response).to have_http_status(:success)
+      expect(response.body).to eql('Successfully deleted')
+    end
+  end
+
+end
