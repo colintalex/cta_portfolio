dd8633b1d616fa735f2f2adb4e436204b07e5967
Colin Alexander <colintalex@gmail.com>
2020-12-09 03:31:33 -0700

    Sets up React admin login basic skeleton and function

--- app/javascript/components/Admin/AdminDashboard.jsx
+++ app/javascript/components/Admin/AdminDashboard.jsx
@@ -1,19 +1,23 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios'
 import AdminLogin from './AdminLogin'
 
 const AdminDashboard = () => {
     const [ isAuthenticated, setIsAuthenticated ] = useState(false)
+    const [ currentAdmin, setCurrentAdmin ] = useState({})
 
     if(isAuthenticated) {
         return (
             <div>
-                Admin Dashbord
+                Admin Dashbord | 
+                Welcome, {currentAdmin.name}!
             </div>
         )
     }else{
         return (
-            <AdminLogin/>
+            <AdminLogin
+                setIsAuthenticated={setIsAuthenticated}
+                setCurrentAdmin={setCurrentAdmin}
+            />
         )
     }
 }
--- app/javascript/components/Admin/AdminLogin.jsx
+++ app/javascript/components/Admin/AdminLogin.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
+import axios from 'axios'
+import { useForm } from 'react-hook-form'
 
-const AdminLogin = () => {
+const AdminLogin = ({ setIsAuthenticated, setCurrentAdmin }) => {
+    const { register, handleSubmit, watch, errors } = useForm();
+
+    const _handleAdminLogin = (data) => {
+        axios.post('/api/v1/admin/login', {
+            email: data.email,
+            password: data.password
+        })
+        .then(data => {
+            setCurrentAdmin(data.data.admin)
+            setIsAuthenticated(true)
+        })
+        .catch(error => console.log(error))
+    }
+
     return (
         <div>
             Login here
-            <form>
-                <input type='text' placeholder='Email' />
-                <input type='password' placeholder='Password'/>
+            <form onSubmit={handleSubmit(_handleAdminLogin)}>
+                <input type='text' name='email' ref={register} placeholder='Email' />
+                <input type='password' name='password' ref={register} placeholder='Password'/>
                 <input type="submit"/>
             </form>
         </div>
