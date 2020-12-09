f15c7344b65067ad156d84d1e3214706323be24d
Colin Alexander <colintalex@gmail.com>
2020-12-09 03:02:00 -0700

    Adds methods for authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,26 @@
 class ApplicationController < ActionController::Base
+    def session_admin
+        decoded_hash = decoded_token
+        if decoded_hash
+            admin_id = decoded_hash[0]['admin_id']
+            @admin = Admin.find_by(id: admin_id)
+        else
+            nil
+        end
+    end
+
+    def auth_header
+        request.headers['Authorization']
+    end
+
+    def decoded_token
+        if auth_header
+            token = auth_header.split(' ')[1]
+            begin
+                JWT.decode(token, 'secret', true, algorithm: 'HS256')
+            rescue JWT::DecodeError
+                []
+            end
+        end
+    end
 end
