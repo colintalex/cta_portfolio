d42d32c62a47f16faaa88d0a0d3da88f6073105a
Colin Alexander <colintalex@gmail.com>
2020-12-09 03:02:52 -0700

    Creates routes for login and auto-login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_06_070313) do
+ActiveRecord::Schema.define(version: 2020_12_09_092403) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2020_12_06_070313) do
   create_table "admins", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.string "token"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
