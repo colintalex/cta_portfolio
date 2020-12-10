08fadc9ca734b44dc8f415ab219bc9c955a942f5
Colin Alexander <colintalex@gmail.com>
2020-12-09 17:58:26 -0700

    Creates skeleton component w/ requests for code proj list

--- app/javascript/components/Admin/Dashboard/CodeProjects/ContainerCodeProjects.jsx
+++ app/javascript/components/Admin/Dashboard/CodeProjects/ContainerCodeProjects.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 import CodeProjectCreate from './CodeProjectCreate'
 import CodeProjectList from './CodeProjectList'
 
 const ContainerCodeProjects = () => {
+    const [ codeProjects, setCodeProjects ] = useState([])
+
+    useEffect(() => {
+        axios.get('/api/v1/code_projects')
+        .then(data => {
+            setCodeProjects(data.data.data)
+        })
+        .catch(error => console.log(error))
+    },[])
 
     return (
         <div>
-            <CodeProjectList/>
+            <CodeProjectList
+                codeProjects={codeProjects}
+            />
             <CodeProjectCreate/>
         </div>
     )
