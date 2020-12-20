0192bfce5aa2a36dccbbdfc3f171ff12ffec6b99
Colin Alexander <colintalex@gmail.com>
2020-12-19 21:55:45 -0700

    Namespace fix addition

---
+++ app/controllers/api/v1/auth/auth_controller.rb
@@ -0,0 +1,29 @@
+class Api::V1::Auth::AuthController < ApplicationController
+    skip_before_action :verify_authenticity_token #Remove after 3rd party request testing (Postman)
+    
+    def login
+        admin = Admin.find_by(email: admin_params[:email])
+        if admin && admin.authenticate(params[:password])
+            payload = {admin_id: admin.id}
+            admin.token = JWT.encode(payload, 'secret') # Hide secret in ENV
+            admin.save
+            render json: AdminSerializer.new(admin)
+        else
+            render json: {errors: 'Credentails failed!'}
+        end
+    end
+
+    def auto_login
+        if session_admin
+            render json: session_admin
+        else
+            render json: {errors: 'No Admin Logged In'}
+        end
+    end
+
+    private
+
+    def admin_params
+        params.permit(:email, :password)
+    end
+end
