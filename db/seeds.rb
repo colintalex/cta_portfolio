e95cb05085fc7a714e766ca86e4a492b11966e50
Colin Alexander <colintalex@gmail.com>
2020-12-21 21:44:14 -0700

    Adds ADmin seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Admin.destroy_all
+
+Admin.create(
+    name: 'Colin',
+    email: 'colintalex@gmail.com',
+    password: '123' 
+)
