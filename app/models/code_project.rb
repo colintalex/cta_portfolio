670099a9fe602f1910e3185358a4f9a3ad414ed0
Colin Alexander <colintalex@gmail.com>
2020-12-22 16:30:31 -0700

    Mirrors changes to the graphicprojects, uploads and displays multiple photos

--- app/models/code_project.rb
+++ app/models/code_project.rb
@@ -1,7 +1,15 @@
 class CodeProject < ApplicationRecord
+    include Rails.application.routes.url_helpers
     validates :title, presence: true, uniqueness: true
     validates :description, presence: true
     validates :github_url, presence: true
     validates :technology, presence: true
     has_many_attached :images
+
+    def get_image_urls
+        urls = self.images.map do |img|
+            rails_blob_path(img)
+        end
+        urls
+    end
 end
