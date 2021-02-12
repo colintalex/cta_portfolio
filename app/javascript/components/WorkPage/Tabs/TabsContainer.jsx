fcf9e847d47b38fa55ff6ba9aaeecf530d7aa4b6
Colin Alexander <colintalex@gmail.com>
2021-02-12 03:04:28 -0700

    Creates tab indicator arrow with animation

--- app/javascript/components/WorkPage/Tabs/TabsContainer.jsx
+++ app/javascript/components/WorkPage/Tabs/TabsContainer.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import CodeProjectModule from '../WorkModules/CodeProjectModule'
 import GraphicProjectModule from '../WorkModules/GraphicProjectModule'
 import styled from 'styled-components'
+import { HiArrowNarrowLeft } from 'react-icons/hi'
 
 const StyledTabs = styled.button`
     font-family: 'Big Shoulders Display', light;
@@ -23,6 +24,7 @@ const StyledTabList = styled.ul`
     display: inline-block;
     text-align: center;
     width: 100%;
+    padding: 0;
 `
 
 const StyledTabItem = styled.li`
@@ -30,15 +32,39 @@ const StyledTabItem = styled.li`
     margin: 0 25px;
 `
 
+const StyledPageHeader = styled.h2`
+    margin: 10px auto;
+    text-align: center;
+    font-size: 2.5em;
+    color: lightgray;
+`
+
+const StyledArrowIcon = styled(HiArrowNarrowLeft)`
+    width: 40px;
+    height: 40px;
+    vertical-align: bottom;
+    color: turquoise;
+
+    transition: transform .6s;
+    transform: ${props => 
+        (props.activeTab == 'tab-code-projects') ? 'rotateZ(0)' : 'rotateZ(180deg)'
+    };
+
+`
+
 const TabsContainer = () => {
     const [ activeTab, setActiveTab ] = useState('tab-code-projects')
 
     return (
         <div className='' id='tabs-container'>
+            <StyledPageHeader>My Personal Work</StyledPageHeader>
             <StyledTabList>
                 <StyledTabItem className='tab' id='tab-code-projects'>
                     <StyledTabs onClick={e => setActiveTab('tab-code-projects')}>Code Work</StyledTabs>
                 </StyledTabItem>
+                    <StyledArrowIcon
+                        activeTab={activeTab}
+                    />
                 <StyledTabItem className='tab' id='tab-graphic-projects'>
                     <StyledTabs onClick={e => setActiveTab('tab-graphic-projects')}>Graphic Design</StyledTabs>
                 </StyledTabItem>
