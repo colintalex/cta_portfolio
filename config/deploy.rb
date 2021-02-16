dcd2e0421f42c2a15fe1cce6c5d9ad1e0ec7bd80
Colin Alexander <colintalex@gmail.com>
2021-02-16 00:02:26 -0700

    a

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.15.0"
 
 set :application, "portfolio-app"
-set :repo_url, "git@github.com:colintalex/cta_portfolio.git"
+set :repo_url, "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDEa1O9bgk4RQ5nxdVKdIqT+Ol2m9xB4Z8SMB40oWRU012qbTp4FfqIHmcZH6sa/QMt2vCB1aswKFQsNwLljrpucCQGD4kNBshVwCThzBEuUNdKPHawNQc0y3Lwg6npl1w+apQ3idIBPIhpOuV/YBRmoR7Kq5fwCuxCdb2hPU4hx1RCikQLMSu/uFyCAC5MHEfmH2M0b8Y3NFuwyPVyQTEQeOUyS1+R3mgJhta/FDZSWMhvoE+9hiKffFBBFkXrJqc98MRm0VoTBdO8eXDzHkXsHs/Z5XJyFO4FjyGVC6l14yGokHgl0FBGNbYKzSDfq298r2/Vzow/m4/i/mHHw2vdM/M+8JTsacDxXFpdGcZWQssU5HdXHXiCPQ8P+Pk+ta9z51D36AJjgUVRniELwhq+Eoh8uJKyJorKq6iYzMw6K73LMZy0UWhxVtPFklJuZSf2ksE3TEFXCIzR8pEfEwDze+WeYGbCxGQVKLR8m7w6PVNGobYHUi4utgh5QH8viL0= colinalexander@Colins-iMac.local: git@github.com:colintalex/cta_portfolio.git"
 
 # Default branch is :main
 ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
