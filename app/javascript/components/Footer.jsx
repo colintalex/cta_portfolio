226149ad2633a649c91bd9d1a00f7d59cf501d78
Colin Alexander <colintalex@gmail.com>
2021-02-21 07:19:01 -0700

    Adds content and links to footer

--- app/javascript/components/Footer.jsx
+++ app/javascript/components/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import StyledIcons from './NavBar/StyledIcons'
 
 const StyledFooter = styled.div`
     background: #1f1f1f;
@@ -7,12 +8,46 @@ const StyledFooter = styled.div`
     border-style: solid;
     border-width: 1px;
     border-top-color: #cc851e94;
+    height: auto;
+    
+    #footer-links {
+        display: inline-block;
+        text-align: end;
+        width: 50%:
+    }
+
+`
+
+const StyledTag = styled.div`
+    display: inline-block;
+    width: 50%;
+    p {
+        padding-left: 25px;
+        color: gray;
+    }
+`
+
+const IconsWrapper = styled.div`
+    width: 50%;
+    display: inline-block;
+    text-align: end;
+
+    svg {
+        height: 30px;
+        width: 30px;
+    }
+
 `
 
 const Footer = () => {
     return(
         <StyledFooter>
-            Footer
+            <StyledTag>
+                <p id='footer-tag'>Colin Alexander 2020</p>
+            </StyledTag>
+            <IconsWrapper>
+                <StyledIcons/>
+            </IconsWrapper>
         </StyledFooter>
     )
 }
