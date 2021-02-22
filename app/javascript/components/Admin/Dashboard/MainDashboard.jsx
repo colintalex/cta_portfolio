ea6b05c89a10036ecd9e04b6f7b339d0afbfc34a
Colin Alexander <colintalex@gmail.com>
2021-02-22 08:34:11 -0700

    Sets up basic admin pw change

--- app/javascript/components/Admin/Dashboard/MainDashboard.jsx
+++ app/javascript/components/Admin/Dashboard/MainDashboard.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
+import AdminSettings from './AdminSettings'
 import ContainerApiProjects from './ApiProjects/ContainerApiProjects'
 import ContainerFullstackProjects from './FullstackProjects/ContainerFullstackProjects'
 
-const MainDashboard = ({ currentAdmin }) => {
+const MainDashboard = ({ currentAdmin, setCurrentAdmin }) => {
 
     return (
         <div>
-            <h4>Main Dashboard</h4>
+            <h2>Main Dashboard</h2>
+            <h4>Welcome {currentAdmin.attributes.name}!</h4>
+            <h4>{currentAdmin.attributes.email}</h4>
             <hr/>
+            <AdminSettings
+                currentAdmin={currentAdmin, setCurrentAdmin}
+            />
             <ContainerApiProjects
                 currentAdmin={currentAdmin}
             /> <hr/>
