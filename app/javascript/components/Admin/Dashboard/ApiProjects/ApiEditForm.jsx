4fb3b9f096f0e3bf79887a2ae8ff3f20511a4d43
Colin Alexander <colintalex@gmail.com>
2021-02-22 07:08:57 -0700

    Adds missing for labels to form

--- app/javascript/components/Admin/Dashboard/ApiProjects/ApiEditForm.jsx
+++ app/javascript/components/Admin/Dashboard/ApiProjects/ApiEditForm.jsx
@@ -20,17 +20,17 @@ const ApiEditForm = ({ currentApiProject, _handleProjectUpdate, setEditMode }) =
     return(
         <div>
             <form onSubmit={handleSubmit(_handleProjectUpdate)}>
-                <label for=''>Title</label>
+                <label for='title'>Title</label>
                 <input type='text' name='title' ref={register} />
-                <label for=''>Description</label>
+                <label for='description'>Description</label>
                 <input type='text' name='description' ref={register} />
-                <label for=''>GitHub</label>
+                <label for='github_url'>GitHub</label>
                 <input type='text' name='github_url' ref={register} />
-                <label for=''>Deployment</label>
+                <label for='deploy_url'>Deployment</label>
                 <input type='text' name='deploy_url' ref={register} />
-                <label for=''>Technology</label>
+                <label for='technology'>Technology</label>
                 <input type='text' name='technology' ref={register} />
-                <label for=''>Challenges</label>
+                <label for='challenges'>Challenges</label>
                 <input type='text' name='challenges' ref={register} />
                 <br/>Select images to delete<br/>
 
